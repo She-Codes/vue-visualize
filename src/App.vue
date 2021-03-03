a9f136fd05052c688b7453faf14ac5153eb082d2
She-Codes <n.y.shaw@gmail.com>
2021-03-03 18:49:54 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input v-model="igUrl" class="border border-indigo-400">
+    <button>Get Post</button>
+  </div>
+</template>
+
+<script setup>
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+  const igUrl = 'link';
+</script>
+
