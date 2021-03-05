3712cbed7a4837d7000055e470bcfecf85cb58d8
She-Codes <n.y.shaw@gmail.com>
2021-03-05 10:13:42 -0500

    removed Vue 3 experimental <script setup>

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,19 @@
   </div>
 </template>
 
-<script setup>
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-  let igUrl = '';
-  let igPost = '';
-
-  const getPost = async () => {
-    igPost = await fetch('.netlify/functions/ig-post').then((r) => r.json());
+<script>
+  export default {
+    data () {
+      return {
+        igUrl: '',
+        igPost: ''
+      }
+    },
+    methods: {
+      async getPost() {
+        this.igPost = await fetch('.netlify/functions/ig-post').then((r) => r.json());
+      }
+    }
   }
 </script>
 
