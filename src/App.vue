5a5e084431e430d008319071cf9867bfa3f3075b
She-Codes <n.y.shaw@gmail.com>
2021-03-04 22:44:20 -0500

    successfully hitting serverless api and getting a response
    nothing displayed on the page yet

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <input v-model="igUrl" class="border border-indigo-400">
-    <button>Get Post</button>
+    <button @click="getPost">Get Post</button>
+
+    <div>
+      {{igPost}}
+    </div>
   </div>
 </template>
 
 <script setup>
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-  const igUrl = 'link';
+  let igUrl = '';
+  let igPost = '';
+
+  const getPost = async () => {
+    igPost = await fetch('.netlify/functions/ig-post').then((r) => r.json());
+  }
 </script>
 
